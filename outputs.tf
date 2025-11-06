dff417a46d3f7854057fd4025466c28b89f86805
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-05 19:34:54 -0500

    commit outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "rg_name" {
+  value = azurerm_resource_group.monitoring.name
+}
+
+output "law_name" {
+  value = azurerm_log_analytics_workspace.law.name
+}
+
+output "law_id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
+
+output "location" {
+  value = azurerm_resource_group.monitoring.location
+}
